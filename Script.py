5c2053bcc97b75bc7102756da7090a7672e158f2
Hariom Singh <168641603+Hariombotz@users.noreply.github.com>
2024-07-13 22:46:41 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -105,7 +105,7 @@ User - {}"""
 ❗️ ʟᴀɴɢᴜᴀɢᴇ - {languages}</b>
 """
 
-    FILE_CAPTION = """<a href='https://t.me/bisal_files'><b>ɴᴀᴍᴇ : {file_name}</b></a>"""
+    FILE_CAPTION = """<a href='https://t.me/hbbotz_supportch'><b>ɴᴀᴍᴇ : {file_name}</b></a>"""
 
     RESTART_TXT = """<b>
 📅 Dᴀᴛᴇ : <code>{}</code>
